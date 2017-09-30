5b2b7cccaf65c6969576b1f57666e18c02d06466
Jesse Tham <jtham@ualberta.ca>
2017-09-29 23:10:51 -0600

    In place processing

--- rle.c
+++ rle.c
@@ -6,19 +6,13 @@
 
 uint32_t rle_compress( uint8_t* data_array, uint32_t data_size )
 {
-	uint8_t* temp_array = calloc( data_size, sizeof( uint8_t ) );
-	if( temp_array == NULL )
-	{
-		return 0;
-	}
-
-	uint32_t data_index = 0, temp_index = 0;
+	uint32_t data_index = 0, new_index = 0;
 
 	while( data_index < data_size )
 	{
 		uint8_t consecutive_counter = 0;
-		temp_array[temp_index++] = data_array[data_index];
-		while( data_array[data_index] == temp_array[temp_index - 1] &&
+		data_array[new_index++] = data_array[data_index];
+		while( data_array[data_index] == data_array[new_index - 1] &&
 			data_index < RLE_MAX_CONSECUTIVE_SIZE &&
 			data_index < data_size )
 		{
@@ -28,21 +22,16 @@ uint32_t rle_compress( uint8_t* data_array, uint32_t data_size )
 
 		if( consecutive_counter == 1 )
 		{
-			temp_array[temp_index - 1] |= RLE_NON_CONSEC_MASK;
+			data_array[new_index - 1] |= RLE_NON_CONSEC_MASK;
 		}
 		else
 		{
-			temp_array[temp_index++] = consecutive_counter;
+			data_array[new_index++] = consecutive_counter;
 		}
 	}
 
-	for( uint8_t i = 0; i < data_size; i++ )
-	{
-		data_array[i] = temp_array[i];
-	}
-	free( temp_array );
 
-	return temp_index * sizeof( uint8_t );
+	return new_index * sizeof( uint8_t );
 }
 
 uint32_t rle_uncompress( uint8_t* data_array, uint32_t data_size )
